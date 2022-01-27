3e6483303814bd7f5bc879b7b12d454ab994b049
Denis-leparteleg <denislepartelegke@gmail.com>
2022-01-27 22:53:46 +0300

    Create logout api endpoint

--- whipapp/urls.py
+++ whipapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', RegistrationAPI.as_view()),
     path('login/', LoginAPI.as_view()),
     path('user/', UserAPI.as_view()),
+    path('logout/', knox_views.LogoutView.as_view(), name='knox_logout')
 ]
