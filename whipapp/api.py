834ae668c65845551b6245a710a5693d7f942c2c
Denis-leparteleg <denislepartelegke@gmail.com>
2022-01-27 22:44:43 +0300

    Create login api endpoint

--- whipapp/api.py
+++ whipapp/api.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, permissions, generics
 from rest_framework.response import Response
 from knox.models import AuthToken
-from .serializers import CreateUserSerializer, UserSerializer
+from .serializers import CreateUserSerializer, UserSerializer, LoginUserSerializer
 
 class RegistrationAPI(generics.GenericAPIView):
     serializer_class = CreateUserSerializer
@@ -10,6 +10,18 @@ class RegistrationAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
+        })
+        
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginUserSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
         return Response({
             "user": UserSerializer(user, context=self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)[1]
--- whipapp/urls.py
+++ whipapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .api import RegistrationAPI
+from .api import RegistrationAPI, LoginAPI
 from knox import views as knox_views
 
 urlpatterns = [
     path('register/', RegistrationAPI.as_view()),
+    path('login/', LoginAPI.as_view()),
 ]
