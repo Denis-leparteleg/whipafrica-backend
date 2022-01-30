df9ba1ed0e27078b00005d73ab6cff22f5b3717b
Denis-leparteleg <denislepartelegke@gmail.com>
2022-01-30 20:18:49 +0300

    Create db superuser

--- whipapp/serializers.py
+++ whipapp/serializers.py
@@ -14,9 +14,9 @@ class CreateUserSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username','email','password')
+        fields = ('id','email','password')
 class LoginUserSerializer(serializers.Serializer):
-    username = serializers.CharField()
+    email = serializers.CharField()
     password = serializers.CharField()
     def validate(self, data):
         user = authenticate(**data)
